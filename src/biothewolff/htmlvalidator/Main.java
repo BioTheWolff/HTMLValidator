a58027b6f8f0be27ae5f0a690942e64d35e802ab
BioTheWolff <contact.biowolf@gmx.fr>
2020-03-23 14:16:08 +0100

    Added a help message and verified everything works

--- src/biothewolff/htmlvalidator/Main.java
+++ src/biothewolff/htmlvalidator/Main.java
@@ -1,18 +1,33 @@
 package biothewolff.htmlvalidator;
 
 import biothewolff.htmlvalidator.core.read.HTMLReader;
+import biothewolff.htmlvalidator.core.Constants;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class Main
 {
+
+    private static final Map<String, Boolean> arguments_map = new HashMap<String, Boolean>(){
+        {
+            put("crash-test", false);
+            put("tree", false);
+        }
+    };
+    private static final Map<String, String> arguments_display = new HashMap<String, String>(){
+        {
+            put("help", "Displays this help message and exits.");
+            put("crash-test", "Throw an exception if the file contains errors and could not be validated.");
+            put("tree", "display the document tree. May not be displayed if crash-test is provided.");
+        }
+    };
+
     public static void main(String[] args) throws Exception
     {
-
-        ArrayList<String> arguments = new ArrayList<>(Arrays.asList(args));
+        // compute and store arguments
+        computeArguments(new ArrayList<>(Arrays.asList(args)));
 
         if (args.length > 0 && args[0] != null)
         {
@@ -37,19 +52,67 @@ public class Main
 
                 reader.displayPreValidationErrors();
 
-                if (arguments.contains("--crash-test") && reader.hasPreValidationErrors())
+                if (arguments_map.get("crash-test") && reader.hasPreValidationErrors())
                 {
                     throw new Exception("Found pre-validation errors.\n");
                 }
 
-                reader.displayDocument(2);
+                if (arguments_map.get("tree")) reader.displayDocument(2);
             }
 
         }
         else
         {
-            // Launch UI
-            System.out.println("test");
+            System.out.println("No file path provided. Aborting.");
         }
+    }
+
+    public static void computeArguments(ArrayList<String> args)
+    {
+        if (args.isEmpty()) return;
+
+        if (args.contains("-h") || args.contains("--help"))
+        {
+            displayHelp();
+            System.exit(0);
+        }
+
+        for (String a : args)
+        {
+            // we take out the prefix
+            String name = a.replace("--", "");
+            if (arguments_map.containsKey(name)) arguments_map.put(name, true);
+        }
+    }
+
+    public static void displayHelp()
+    {
+
+        // Build the optional args strings
+        StringBuilder compact_opt_args = new StringBuilder();
+        StringBuilder smooth_opt_args = new StringBuilder();
+
+        for (String arg : arguments_display.keySet())
+        {
+            String val = arguments_display.get(arg);
+
+            compact_opt_args.append(String.format("[--%s] ", arg));
+            smooth_opt_args.append(String.format("    %s - %s\n", arg, val));
+        }
+
+        // Build the general display message
+        String sb = String.format("%s v%s - Licensed under %s\n", Constants.name, Constants.version, Constants.license) +
+                String.format("  Author: %s\n", Constants.author) +
+                String.format("  %s\n\n", Constants.description) +
+                String.format("java -jar HTMLValidator.jar <filename> %s\n\n", compact_opt_args.toString()) +
+                "Positional argument:" +
+                "\n" +
+                "    filename - the name of the file to validate" +
+                "\n" +
+                "Optional arguments:\n" +
+                smooth_opt_args.toString();
+        System.out.println(sb);
+
+
     }
 }
---
+++ src/biothewolff/htmlvalidator/core/Constants.java
@@ -0,0 +1,13 @@
+package biothewolff.htmlvalidator.core;
+
+public class Constants {
+
+    public static final String version = "v1.0";
+
+    public static final String name = "HTML Validator";
+    public static final String license = "AGPL-3.0";
+    public static final String description = "A small HTML validator in java. Still WIP";
+    public static final String author = "Fabien \"BioTheWolff\" Z.";
+
+
+}
